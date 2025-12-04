15dbe77e41158f684abac7a70b443240be0f3bb5
Alamin Usman Saleh <alaminusmansaleh43@gmail.com>
2025-12-04 07:53:47 +0100

    fix allowed hosts

--- settings/development.py
+++ settings/development.py
@@ -6,7 +6,7 @@ from .base import *
 
 # Debug settings
 DEBUG = True
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0', '.serveo.net', '.lhr.life']
+# ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0', '.serveo.net', '.lhr.life']
 
 
 
